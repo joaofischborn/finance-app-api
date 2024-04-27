7286a0375f9ed7ebd7953cf478531027ee62c28e
Joao Vitor <joaovitorfischborn@gmail.com>
2024-04-27 19:12:59 -0300

    feat: add CreateUserController and validations

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import 'dotenv/config.js'
 import express from 'express'
+import { CreateUserController } from './src/controllers/create-user.js'
 
 const app = express()
 
 app.use(express.json())
+
+app.post('/api/users', async (req, res) => {
+    const createUserController = new CreateUserController()
+    const { statusCode, body } = await createUserController.execute(req)
+
+    res.status(statusCode).send(body)
+})
 
 app.listen(3000, () => console.log('listening on port 3000'))
